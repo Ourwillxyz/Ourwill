aac0953103424e3f505da6dcf4c2594125acf5d8
Ourwillxyz <admin@ourwill.xyz>
2025-07-04 21:24:54 +0300

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -15,8 +15,8 @@ export default function Navbar() {
         <Image
           src="/ourwill-logo.png"
           alt="OurWill Logo"
-          width={40}
-          height={40}
+          width={80}
+          height={80}
           style={{ borderRadius: '50%' }}
         />
       </Link>
