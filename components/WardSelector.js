767edcaa7ac25b3a080eaabdf65ff1dfc4acfc42
Ourwillxyz <admin@ourwill.xyz>
2025-07-04 21:49:32 +0300

    Update WardSelector.js

--- components/WardSelector.js
+++ components/WardSelector.js
@@ -1,8 +1,7 @@
 export const wards = [
   { name: "Likoni Ward", subcounty: "Likoni", county: "Mombasa" },
   { name: "Timbwani Ward", subcounty: "Likoni", county: "Mombasa" },
-  { name: "Nyalenda A", subcounty: "Kisumu Central", county: "Kisumu" },
   { name: "Langata Ward", subcounty: "Langata", county: "Nairobi" },
+  { name: "Nyalenda A", subcounty: "Kisumu Central", county: "Kisumu" },
   { name: "Milimani Ward", subcounty: "Nakuru Town East", county: "Nakuru" }
-  // Add more here, or later we fetch full IEBC list
 ];
