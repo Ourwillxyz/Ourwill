46b18027b2d55d88a04f62cad6f78743699ba06a
Ourwillxyz <admin@ourwill.xyz>
2025-07-06 11:49:50 +0300

    Delete components/WardSelector.js

--- components/WardSelector.js
+++
@@ -1,7 +0,0 @@
-export const wards = [
-  { name: "Likoni Ward", subcounty: "Likoni", county: "Mombasa" },
-  { name: "Timbwani Ward", subcounty: "Likoni", county: "Mombasa" },
-  { name: "Langata Ward", subcounty: "Langata", county: "Nairobi" },
-  { name: "Nyalenda A", subcounty: "Kisumu Central", county: "Kisumu" },
-  { name: "Milimani Ward", subcounty: "Nakuru Town East", county: "Nakuru" }
-];
