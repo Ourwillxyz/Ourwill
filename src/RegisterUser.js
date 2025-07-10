588241ebd5197d8e6bc59d72d47d145dce1697fa
Ourwillxyz <admin@ourwill.xyz>
2025-07-10 13:17:33 +0300

    Update RegisterUser.js
    Updated RegisterUser.js V13

--- src/RegisterUser.js
+++ src/RegisterUser.js
@@ -153,7 +153,9 @@ const RegisterUser = () => {
             County:<br />
             <select value={selectedCounty} onChange={e => setSelectedCounty(e.target.value)} required style={{ padding: 8, width: '90%', margin: '1rem 0' }}>
               <option value="">--Select County--</option>
-              {counties.map(c => <option key={c.county_code} value={c.county_code}>{c.name}</option>)}
+              {counties.map(c => (
+                <option key={c.code} value={c.code}>{c.name}</option>
+              ))}
             </select>
           </label>
           <br />
@@ -161,7 +163,9 @@ const RegisterUser = () => {
             Subcounty:<br />
             <select value={selectedSubcounty} onChange={e => setSelectedSubcounty(e.target.value)} required disabled={!selectedCounty} style={{ padding: 8, width: '90%', margin: '1rem 0' }}>
               <option value="">--Select Subcounty--</option>
-              {subcounties.map(s => <option key={s.subcounty_code} value={s.subcounty_code}>{s.name}</option>)}
+              {subcounties.map(s => (
+                <option key={s.subcounty_code} value={s.subcounty_code}>{s.name}</option>
+              ))}
             </select>
           </label>
           <br />
@@ -169,7 +173,9 @@ const RegisterUser = () => {
             Ward:<br />
             <select value={selectedWard} onChange={e => setSelectedWard(e.target.value)} required disabled={!selectedSubcounty} style={{ padding: 8, width: '90%', margin: '1rem 0' }}>
               <option value="">--Select Ward--</option>
-              {wards.map(w => <option key={w.ward_code} value={w.ward_code}>{w.name}</option>)}
+              {wards.map(w => (
+                <option key={w.ward_code} value={w.ward_code}>{w.name}</option>
+              ))}
             </select>
           </label>
           <br />
@@ -177,7 +183,9 @@ const RegisterUser = () => {
             Polling Centre:<br />
             <select value={selectedPollingCentre} onChange={e => setSelectedPollingCentre(e.target.value)} required disabled={!selectedWard} style={{ padding: 8, width: '90%', margin: '1rem 0' }}>
               <option value="">--Select Polling Centre--</option>
-              {pollingCentres.map(p => <option key={p.id} value={p.id}>{p.name}</option>)}
+              {pollingCentres.map(p => (
+                <option key={p.id} value={p.id}>{p.name}</option>
+              ))}
             </select>
           </label>
           <br />
