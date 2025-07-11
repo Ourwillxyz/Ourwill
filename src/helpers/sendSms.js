1626cf565afab3cd718ac680992bd33e054bc78f
Ourwillxyz <admin@ourwill.xyz>
2025-07-11 19:21:47 +0300

    Delete src/helpers/sendSms.js

--- src/helpers/sendSms.js
+++
@@ -1,8 +0,0 @@
-export async function sendSms(to, message) {
-  const response = await fetch('http://localhost:5000/send-sms', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ to, message })
-  });
-  return response.json();
-}
