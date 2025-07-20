50a948ef79945f55f2d517cac30098575e80d15b
Ourwillxyz <admin@ourwill.xyz>
2025-07-20 16:07:11 +0300

    Update RegisterUser.js

--- pages/RegisterUser.js
+++ pages/RegisterUser.js
@@ -141,7 +141,7 @@ export default function RegisterUser() {
     const { error } = await supabase.auth.signInWithOtp({
       email: formData.email,
       options: {
-        emailRedirectTo: ${window.location.origin}/dashboard,
+        emailRedirectTo: `${window.location.origin}/dashboard`,
         data: {
           mobile: formData.mobile,
           username: formData.username,
